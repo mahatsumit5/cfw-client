d341a0833ec604969ff51f5125768712d486c83d
mahat sumit <mahatsumit5@gmail.com>
2023-07-26 22:19:41 +1000

    user verification of client side completed

--- src/pages/verifyEmail/verifyEmail.js
+++ src/pages/verifyEmail/verifyEmail.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Footer } from "../../components/layout/Footer";
 import { Header } from "../../components/layout/Header";
 import { useLocation, useNavigate } from "react-router-dom";
-import { Button } from "react-bootstrap";
+import { Button, Form, InputGroup } from "react-bootstrap";
 import { verifyAccountAction } from "../../action/userAction";
 export const VerifyEmail = () => {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const code = queryParams?.get("c");
   const email = queryParams?.get("e");
+  const [form, setForm] = useState({});
   const userName = email?.slice(0, email.indexOf("@"));
   const navigate = useNavigate();
-
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+      email: email,
+    });
+  };
   const handleOnVerify = async () => {
-    const result = await verifyAccountAction({ code, email });
+    const result = await verifyAccountAction(form);
     result && navigate("/");
   };
   return (
@@ -29,6 +37,16 @@ export const VerifyEmail = () => {
             continue, we need to verify your email address.
           </p>
           <div className="d-grid">
+            <Form>
+              <p>Enter your code below</p>
+              <Form.Control
+                id="code"
+                type="text"
+                name="code"
+                onChange={handleOnChange}
+              />
+              <label htmlFor="code">Enter your code</label>
+            </Form>
             <Button onClick={handleOnVerify}>Activate My Account</Button>
           </div>
 
