fd5976311c1a29bd05abacba1668582589960521
mahat sumit <mahatsumit5@gmail.com>
2023-10-01 15:09:25 +1100

    prcess env changed

--- src/pages/checkout/StripeCheckout.js
+++ src/pages/checkout/StripeCheckout.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { Elements, PaymentElement } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
-import { Button } from "@mui/material";
 import CheckoutForm from "./CheckoutForm";
 
+const stripePromise = loadStripe(
+  `${process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}`
+);
 export const StripeCheckout = () => {
-  const stripePromise = loadStripe(
-    `pk_test_51NsR1aGbch8SC1C7zK7H1XG82rBH02wjhXVRsdhOkCsCD0vXSsZD2kzyqncayXn1G4Z7u0trH4Eftu4L3YwnQrmE00GkHj5VOX`
-  );
   console.log(stripePromise);
   return (
     <Elements stripe={stripePromise}>
